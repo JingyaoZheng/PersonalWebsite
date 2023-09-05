8ad258eee87faf98b43b9ff3cece61505faf3abc
Harrison Zheng <74079663+JingyaoZheng@users.noreply.github.com>
2023-09-06 01:00:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Researcher", "Key Online Influencer", "Lifelong Leaner"],
+    typeSpeed:150,
+    backSpeed:50,
+    backDelay:800,
+    loop:true
+});
